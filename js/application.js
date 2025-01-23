61eccca942a98718de8e8996ff62faa224efbdf3
Cody Hookey <codyhookey@outlook.com>
2025-01-23 15:12:39 +0000

    Subtotal feature finished and functional

--- js/application.js
+++ js/application.js
@@ -8,17 +8,28 @@ var updateCost = function (element) {
     return cost;
 };
 
-$(document).ready(function () {
-    $('tbody tr').each(function (i, element) {
-        var cost = updateCost(element);
+var updateSubtotal = function () {
+    var subtotal = 0;
+
+    $('tbody tr').each(function () {
+        var cost = updateCost(this);
+        subtotal += cost;
     });
+
+    $('div h3 #totalValue').html(subtotal.toFixed(2));
+}
+
+$(document).ready(function () {
+    updateSubtotal();
 
     $('tbody').on('input', 'tr input', function () {
         var row = $(this).closest('tr');
         updateCost(row);
+        updateSubtotal();
     });
 
     $(document).on("click", ".btn-remove", function (event) {
         $(this).closest("tr").remove();
+        updateSubtotal();
     });
 });
